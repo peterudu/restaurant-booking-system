fe97124bbea7ef19901f343e48d7b9c7460e572c
peterudu <puduevborho7@gmail.com>
2023-10-31 23:05:21 +0000

    Add Booking Model content to models.py file

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Booking(models.Model):
+    """
+    Model represents a restaurant 0booking made by a user
+    A connection exists between the user and the booked table
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_bookings")
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=150, blank=True, null=True)
+    date = models.DateField()
+    time = models.IntegerField()
+    number_of_guests = models.IntegerField()
+    date = models.DateField()
+
