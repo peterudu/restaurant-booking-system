05f944f67c171d2ad9f54d0474fbb17b72d2cd14
peterudu <puduevborho7@gmail.com>
2023-11-08 22:48:08 +0000

    Add decorator to BookAdmin class in admin.py file

--- booking/admin.py
+++ booking/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Booking
 
-admin.site.register(Booking)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    model = Booking
+    list_display = ('name', 'email')
